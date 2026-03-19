49d68a6f17920bb9717b8d76113896eb72286fe5
Matheus Assis <matheusassis488@gmail.com>
2026-03-18 21:38:07 -0300

    Add Intersection Of Two Linked List (LeetCode 160) solution

---
+++ src/leetcode/IntersectionOfTwoLinkedList.java
@@ -0,0 +1,19 @@
+public class IntersectionOfTwoLinkedList {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) {return null;}
+
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        while(pA != pB) {
+
+            if(pA == null) {pA = headB;} 
+            else {pA = pA.next;} 
+            
+            if(pB == null) {pB = headA;} 
+            else {pB = pB.next;}
+        }
+
+        return pA;
+    }
+}
